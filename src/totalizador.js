5bb5de5fdbb07709f07c4f0563809d23e86ec53a
Fabio Andre <fabio.mojica@ucb.edu.bo>
2024-02-29 09:43:21 -0400

    Se agrego funcionalidad para sacar el precio neto + impuesto (CA)

---
+++ src/totalizador.js
@@ -0,0 +1,10 @@
+function totalizador(precio,cantidad, eCodigo){
+    if(eCodigo === "CA"){
+        let precioNeto = precio * cantidad;
+        let tax = 0.0875  * precioNeto;
+        return tax + precioNeto;
+    } 
+    return precio * cantidad;
+}
+
+export default totalizador;
--- src/totalizador.spec.js
+++ src/totalizador.spec.js
@@ -1,11 +1,11 @@
-//import sumar from "./sumador.js";
+import totalizador from "./totalizador";
 
 describe("Totalizador", () => {
   it("deberia mostrar el precio neto (2)a partir de la cant y precio", () => {
     expect(totalizador(1,2)).toEqual(2);
   });
-});
 
-function totalizador(precio,cantidad){
-    return precio * cantidad;
-}
+  it("Deberia mostrar el precio neto con el impuesto aplicado a CA", () =>{
+    expect(totalizador(1,2, "CA")).toEqual(2.175);
+  });
+});
