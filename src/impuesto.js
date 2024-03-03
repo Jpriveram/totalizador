3effe5968408ee6b628f0be66f5046d20153972e
Fabio Andre <fabio.mojica@ucb.edu.bo>
2024-03-03 01:53:40 -0400

    Refactory del impuesto.js, simplificando codigo

--- src/impuesto.js
+++ src/impuesto.js
@@ -2,27 +2,8 @@ import codimpuesto from "./codImpuesto.js";
 import catimpuesto from "./catImpuesto.js";
 
 function impuesto(precio,cantidad,codigo,categoria) {
-    if (categoria === "Bebidas alcholicas") {
-        let precioNeto = precio * cantidad;
-        let tax = ((codimpuesto(codigo) + catimpuesto(categoria)) / 100 )* precioNeto;
-        return tax ;
-        
-    } else 
-    {
-        if (categoria === "Muebles") {
-            let precioNeto = precio * cantidad;
-            let tax = ((codimpuesto(codigo) + catimpuesto(categoria)) / 100)* precioNeto;
-            return tax ;
-            
-        } else 
-        {
-            let precioNeto = precio * cantidad;
-            let tax = (codimpuesto(codigo)  / 100)* precioNeto;
-            return tax ;
-        }
-    }
-    
-    
-
+    let precioNeto = precio * cantidad;
+    let tax = ((codimpuesto(codigo) + catimpuesto(categoria)) / 100 )* precioNeto;
+    return tax ;
 }
 export default impuesto;
