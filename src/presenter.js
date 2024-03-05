95b2d3b77b417a929ace45c9b71a9bafe9d2d9d7
Juan Pablo Rivera Mercado <jpriveram01@gmail.com>
2024-03-04 21:30:51 -0400

    Se alertas para parametros con numeros negativos o vacios

--- src/presenter.js
+++ src/presenter.js
@@ -9,7 +9,7 @@ import descuento from "./descuento.js";
 import envio from "./peso.js";
 import imprimirDescuentoPorTipoCliente from "./imprimirDescTipoCliente.js";
 import descuentoEnvio from "./descuentoEnvio.js";
-import descuentosEspeciales from "./descuentosEspeciales.js";
+
 
 const price = document.querySelector("#precio");
 const amount = document.querySelector("#cantidad");
@@ -33,18 +33,24 @@ form.addEventListener("submit", (event) => {
   const ValueCat = selCat.value;
   const valueCliente = cliente.value;
 
-
+  if (isNaN(pNumber) || isNaN(cNumber) || isNaN(wnumber)) {
+    alert("Por favor, ingrese números válidos en todos los campos, nose permiten campos vacios.");
+    return;
+  }
+  if (pNumber < 0 || cNumber < 0 || wnumber < 0 ) {
+    alert ("No se permiten números negativos en los campos.");
+    return;
+  }
   div.innerHTML = "<p>" + 
-  "Precio producto: "+ pNumber + "<br>" +
-  "Cantidad producto: "+ cNumber + "<br>" +
-  "Precio neto: "+ (pNumber * cNumber) + "<br>" +
-  "Impuesto para el estado " + selectedValue + "(" + codimpuesto(selectedValue) + "%)  + impuesto para " + ValueCat + "(" + catimpuesto(ValueCat) + "%): "+ impuesto(pNumber,cNumber,selectedValue,ValueCat) + "<br>" +
+  "Precio producto: "+ pNumber + "$" + "<br>" +
+  "Cantidad producto: "+ cNumber + "uds" + "<br>" +
+  "Precio neto: "+ (pNumber * cNumber) + "$" + "<br>" +
+  "Impuesto para el estado " + selectedValue + "(" + codimpuesto(selectedValue) + "%)  + impuesto para " + ValueCat + "(" + catimpuesto(ValueCat) + "%): "+ "+" + impuesto(pNumber,cNumber,selectedValue,ValueCat) + "$" +"<br>" +
   "Descuento por cantidad de " + cNumber + " unidades es de: " + imprimirDescuentoOrden(cNumber) + " del precio total " + 
-  "+ descuento adicional por categoria " + ValueCat + "(" + imprimirDescAdicCat(ValueCat) + "):" + descuento(pNumber, cNumber, ValueCat) + "<br>" + 
-  "Precio de envio: " + envio(wnumber,cNumber) + "$" + "<br>" +
+  "+ descuento adicional por categoria " + ValueCat + "(" + imprimirDescAdicCat(ValueCat) + "): " + "- " + descuento(pNumber, cNumber, ValueCat) + "$" +"<br>" + 
+  "Precio de envio: " + "+" +envio(wnumber,cNumber) + "$" + "<br>" +
   "Tipo de cliente: " + valueCliente + "<br>" + 
-  "Descuento en envio por ser cliente " + valueCliente + " es del " + imprimirDescuentoPorTipoCliente(valueCliente) + " del costo del envio: " + descuentoEnvio(valueCliente, envio(wnumber,cNumber)) + "<br>" +
-  "Descuento especial: " + descuentosEspeciales(valueCliente, ValueCat, (pNumber * cNumber)) + "<br>" +
-  "Precio neto: " + totalizador(pNumber,cNumber,selectedValue,ValueCat,wnumber, valueCliente) + 
+  "Descuento en envio por ser cliente " + valueCliente + " es del " + imprimirDescuentoPorTipoCliente(valueCliente) + " del costo del envio: " + "- " + descuentoEnvio(valueCliente, envio(wnumber,cNumber)) + "$" + "<br>" +
+  "Precio Total: " + totalizador(pNumber,cNumber,selectedValue,ValueCat,wnumber, valueCliente) + "$" +
   "</p>";
 });
